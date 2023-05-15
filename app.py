7d41c3b71158641859d36ce7ab941684fa718a30
Morgan Mendis <morganandrewmendis@gmail.com>
2023-05-15 10:40:26 -0500

    Update error handling

--- app.py
+++ app.py
@@ -256,32 +256,36 @@ print(f"postgresql+psycopg2://{db_username}:{db_password}@{db_host}:5432/{db_nam
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        input_text = request.form['input_text']
-        if request.form['input_type'] == 'question':
-            # Generate SQL from question using OpenAI's GPT-3 API
-            #prompt = f"Convert this natural language query into SQL: {input_text}"
-            prompt = openai_prompt + input_text + "\n\ Use limit clause to only return a maximum of 100 rows and write the SQL in the postgresql syntax.\nSQL:"
-            model = "text-davinci-003"
-            response = openai.Completion.create(
-                engine=model,
-                prompt=prompt,
-                max_tokens=824,
-                n=1,
-                stop=None,
-                temperature=0.5,
-            )
-            sql = response.choices[0].text.strip()
+    try:
+        if request.method == 'POST':
+            input_text = request.form['input_text']
+            if request.form['input_type'] == 'question':
+                # Generate SQL from question using OpenAI's GPT-3 API
+                #prompt = f"Convert this natural language query into SQL: {input_text}"
+                prompt = openai_prompt + input_text + "\n\ Use limit clause to only return a maximum of 100 rows and write the SQL in the postgresql syntax.\nSQL:"
+                model = "text-davinci-003"
+                response = openai.Completion.create(
+                    engine=model,
+                    prompt=prompt,
+                    max_tokens=824,
+                    n=1,
+                    stop=None,
+                    temperature=0.5,
+                )
+                sql = response.choices[0].text.strip()
+            else:
+                # Use SQL entered by the user
+                sql = request.form['input_text']
+            sql = sql.replace('\r', '').replace('\n', ' ')
+            with engine.connect() as conn:
+                results = conn.execute(text(sql)).fetchall()
+            rows_dict = [row._asdict() for row in results]
+            return render_template('index.html', results=rows_dict, input_text=input_text, sql=sql)
         else:
-            # Use SQL entered by the user
-            sql = request.form['input_text']
-        sql = sql.replace('\r', '').replace('\n', ' ')
-        with engine.connect() as conn:
-            results = conn.execute(text(sql)).fetchall()
-        rows_dict = [row._asdict() for row in results]
-        return render_template('new_index.html', results=rows_dict, input_text=input_text, sql=sql)
-    else:
-        return render_template('new_index.html')
+            return render_template('index.html')
+    except Exception as e:
+        # Render the template with the SQL error
+        return render_template('error.html', error=str(e),query=sql)
 
 # Route to '/data_dict' that will render the data_dict.html template based on the data submitted from the form
 @app.route('/data_dict', methods=['GET', 'POST'])
