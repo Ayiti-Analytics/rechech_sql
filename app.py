fcbe9feffeec6845f17338204ef6a24bae7f08b7
Morgan Mendis <morganandrewmendis@gmail.com>
2023-05-28 17:17:48 -0500

    Updated the question feature to use langchain instead of just open AI

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.agents import load_tools
-
+from langchain import SQLDatabase, SQLDatabaseChain
 
 
 # Create an instance of Flask
@@ -32,39 +32,17 @@ db_host = os.environ.get('DB_HOST')
 # Load database name from environment variable
 db_name = os.environ.get('DB_NAME')
 
-db_port = 5432
-engine = create_engine(f"redshift+psycopg2://{db_username}:{db_password}@{db_host}:5439/{db_name}")
-
-
+db_port = 5439
+db_uri = f"redshift+psycopg2://{db_username}:{db_password}@{db_host}:5439/{db_name}"
+engine = create_engine(db_uri)
 
 # Load OpenAI API key from environment variable
 openai.api_key = os.environ.get("OPENAI_API")
 
 
-llm = OpenAI(openai_api_key=os.environ.get("OPENAI_API"),max_tokens=800, n=1, stop=None, temperature=0.2,model = "text-davinci-003")
-
-
-# Load OpenAI prompt from text file
-#with open("openai_prompt.txt") as f:
-#    openai_prompt = f.read()
-
-openai_prompt = """
-Given the following Redshift database tables:
-
-- producer_prices_hti
--infrastructure_hti
-
-
-Convert this natural language query into SQL based on the tables defined above in the database:
-{question}
-\n\ Use limit clause return a maximum of 100 rows and write the SQL in the postgresql syntax. Write the SQL with the fully qualified column names so there is no ambiguity.\nPostgres SQL:
-"""
-prompt_temp = PromptTemplate(
-    input_variables=["question"],
-    template=openai_prompt,
-)
-
-chain = LLMChain(llm = llm, prompt=prompt_temp)
+llm = OpenAI(openai_api_key=os.environ.get("OPENAI_API"), temperature=0)
+db = SQLDatabase.from_uri(db_uri)
+db_chain = SQLDatabaseChain(llm=llm,database=db,verbose=True,use_query_checker=True, return_intermediate_steps=True)
 
 # Get the absolute path to the "data" directory
 data_dir = os.path.abspath("data")
@@ -77,41 +55,49 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    try:
-        if request.method == 'POST':
-            input_text = request.form['input_text']
-            if request.form['input_type'] == 'question':
-                # Generate SQL from question using OpenAI's GPT-3 API
-                #prompt = f"Convert this natural language query into SQL: {input_text}"
-                #prompt = openai_prompt + input_text + "\n\ Use limit clause return a maximum of 100 rows and write the SQL in the postgresql syntax. Write the SQL with the fully qualified column names so there is no ambiguity.\nPostgres SQL:"
-                #model = "text-davinci-003"
-                """
-                response = openai.Completion.create(
-                    engine=model,
-                    prompt=prompt,
-                    max_tokens=800,
-                    n=1,
-                    stop=None,
-                    temperature=0.2,
-                )
-                """
-                #response = llm(prompt)
-                #sql = response#.choices[0].text.strip()
-                sql = chain.run(input_text)
-
-            else:
+    if request.method == 'POST':
+        input_text = request.form['input_text']
+        if request.form['input_type'] == 'question':
+            # Generate SQL from question using OpenAI's GPT-3 API
+            #prompt = f"Convert this natural language query into SQL: {input_text}"
+            #prompt = openai_prompt + input_text + "\n\ Use limit clause return a maximum of 100 rows and write the SQL in the postgresql syntax. Write the SQL with the fully qualified column names so there is no ambiguity.\nPostgres SQL:"
+            #model = "text-davinci-003"
+            """
+            response = openai.Completion.create(
+                engine=model,
+                prompt=prompt,
+                max_tokens=800,
+                n=1,
+                stop=None,
+                temperature=0.2,
+            )
+            """
+            #response = llm(prompt)
+            #sql = response#.choices[0].text.strip()
+            try:
+                response = db_chain(input_text)
+                ai_result = response['result']
+                steps = response['intermediate_steps']
+                string_steps = [i for i in steps if type(i)==str]
+                return render_template('index.html', ai_result=ai_result, input_text=input_text,data = steps )
+            except Exception as e:
+                # Render the template with the SQL error
+                return render_template('error.html', error=str(e))
+        else:
+            try:
                 # Use SQL entered by the user
                 sql = request.form['input_text']
-            sql = sql.replace('\r', '').replace('\n', ' ')
-            with engine.connect() as conn:
-                results = conn.execute(text(sql)).fetchall()
-            rows_dict = [row._asdict() for row in results]
-            return render_template('index.html', results=rows_dict, input_text=input_text, sql=sql)
-        else:
-            return render_template('index.html')
-    except Exception as e:
-        # Render the template with the SQL error
-        return render_template('error.html', error=str(e),query=sql)
+                sql = sql.replace('\r', '').replace('\n', ' ')
+                with engine.connect() as conn:
+                    results = conn.execute(text(sql)).fetchall()
+                rows_dict = [row._asdict() for row in results]
+                return render_template('index.html', results=rows_dict, input_text=input_text, sql=sql)
+            except Exception as e:
+                # Render the template with the SQL error
+                return render_template('error.html', error=str(e),query=sql)
+    else:
+        return render_template('index.html')
+    
 
 # Route to '/data_dict' that will render the data_dict.html template based on the data submitted from the form
 """
